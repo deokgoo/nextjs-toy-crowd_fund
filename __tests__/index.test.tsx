7fcec7e03ab3d25ece971be222cdf65f5a0e6d28
deokgoo <kkddgg1001@gmail.com>
2021-03-13 19:58:32 +0900

    react init

--- __tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { shallow } from "enzyme";
-import App from "../pages/index";
-
-describe("App", () => {
-  it("renders without crashing", () => {
-    const wrapped = shallow(<App/>);
-    expect(wrapped.find('h1').text()).toEqual(expect.stringContaining('Welcome'));
-  });
-});
--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }: {Component: React.FunctionComponent, pageProps: any}) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
