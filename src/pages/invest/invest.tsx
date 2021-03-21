8a583f3ee08c7f4a81ea138ac3aed612fb549dee
dyjung123 <jeongdongryun@gmail.com>
2021-03-21 21:36:52 +0900

    feat: Add enter fixed amount and msg

--- src/pages/invest/invest.tsx
+++ src/pages/invest/invest.tsx
@@ -6,6 +6,7 @@ import styles from './invest.module.scss';
 const Invest = () => {
   const [info, setInfo] = useState<any>();
   const inputRef = useRef<HTMLInputElement>(null);
+  const msgRef = useRef<HTMLTextAreaElement>(null);
   const history = useHistory();
   const params = useParams<{id: string}>();
 
@@ -16,30 +17,44 @@ const Invest = () => {
   }
   const onClick = async () => {
     try {
-      await deposit({payload: {fid: params.id,money: inputRef.current?.value}});
+      await deposit({payload: { fid: params.id, money: inputRef.current?.value, msg: msgRef.current?.value }});
       history.push(`/funding/${params.id}`);
     } catch(err) {
       console.log(err);
     }
-
+  }
+  const addMoney = async (money: number) => {
+    if (!inputRef.current) return;
+    const sum = inputRef.current.value.match(/\d+/) ? parseInt(inputRef.current.value) + money : money;
+    inputRef.current.value = String(sum);
   }
   useEffect(() => {
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <>
+  return (
     <div className={styles.invest}>
       <div className={styles.container}>
         <h1 className={styles.title}>{info?.title}</h1>
         <h2 className={styles.desc}>{info?.desc}</h2>
         <div className={styles.form}>
-          <input className={styles.input} type="text" ref={inputRef}/>
+          <input className={styles.input} type="number" min="0" ref={inputRef}/>
           <div className={styles.unit}>円</div>
+        </div>
+        <div className={styles.amountContainer}>
+          <button className={styles.amount} onClick={() => addMoney(1000)}>+ 1,000</button>
+          <button className={styles.amount} onClick={() => addMoney(5000)}>+ 5,000</button>
+          <button className={styles.amount} onClick={() => addMoney(10000)}>+ 10,000</button>
+          <button className={styles.amount} onClick={() => addMoney(20000)}>+ 20,000</button>
+        </div>
+        <div className={styles.msgContainer}>
+          <h3 className={styles.msgTitle}>Message</h3>
+          <textarea name="msg" id="msg" className={styles.msg} />
         </div>
         <button className={styles.btn} onClick={onClick}>入金</button>
       </div>
     </div>
-  </>
+  );
 }
 
 export default Invest;
